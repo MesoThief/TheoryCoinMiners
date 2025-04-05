195137b1840b511f9e442bc96426b1cebf42b472
박천규 <pckyu01@naver.com>
2025-04-05 15:39:40 +0000

    feat: add xy_tree test input

--- src/xy_tree.cpp
+++ src/xy_tree.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <sstream>
 
 #include "data/Trees.h"
+#include "utils/Alphabet.h"
 
 using namespace std;
 
@@ -22,31 +24,46 @@ void printTree(const shared_ptr<Node>& node, const string& w, string indent = ""
 // ------------------
 // A simple test driver for tree
 // ------------------
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "You must enter a test input file" << endl;
+        cerr << "Usage: " << argv[0] << " <test-input-file-name>" << endl;
+        return 1;
+    }
+
+    string inputFileName = argv[1];
+    ifstream inputFile(inputFileName);
+    if (!inputFile) {
+        cerr << "Error opening " << inputFileName << endl;
+        return 1;
+    }
+
+    // read inputs
+    string alphabet;
     string t;
-    cout << "Enter the value of text t: ";
-    cin >> t;
-    
-    int as;
-    cout << "Enter the value of alphabet size: ";
-    cin >> as;
-    
     string p;
-    int universality;
     int k;
-    cout << "Enter the value of pattern p: ";
-    cin >> p;
-    cout << "Enter the value of k: ";
-    cin >> k;
     
-    RankerTable rankers = RankerTable(t, as);
+    getline(inputFile, alphabet);
+    Alphabet::getInstance().setAlphabet(alphabet);
+
+    getline(inputFile, t);
+    
+    getline(inputFile, p);
+    
+    string line;
+    getline(inputFile, line);
+    istringstream k_stream(line);
+    k_stream >> k;
+    
+    RankerTable rankers = RankerTable(t, Alphabet::getInstance().size());
     rankers.buildXRankerTable();
     rankers.buildYRankerTable();
 
     ShortlexResult shortlex = computePartialShortlexNormalForm(
         p,
-        vector<int>(26, 1),
-        vector<int>(26 ,1),
+        vector<int>(Alphabet::getInstance().size(), 1),
+        vector<int>(Alphabet::getInstance().size() ,1),
         k
     );
 
