4ff8d914ebc147caa8bcd59b394bf7d233425ed2
박천규 <pckyu01@naver.com>
2025-04-10 17:08:47 +0000

    fix: edge cases

--- src/data/Trees.cpp
+++ src/data/Trees.cpp
@@ -11,9 +11,9 @@
  * @return `std::shared_ptr<Node>` Root of the tree.
  */
 std::shared_ptr<Node> Trees::buildXTree(const RankerTable& ranker, const ShortlexResult& shortlex, const std::string& text) {
-    std::shared_ptr<Node> root = std::make_shared<Node>(std::numeric_limits<int>::max(), std::numeric_limits<int>::max(), 0);
+    std::shared_ptr<Node> root = std::make_shared<Node>(RankerTable::INF, RankerTable::INF, 0);
     std::unordered_map<int, std::shared_ptr<Node>> nodes;
-    nodes.insert({{std::numeric_limits<int>::max(), root}});
+    nodes[RankerTable::INF] = root;
     
     std::cout << "Building X-tree...\n";
 
@@ -41,7 +41,7 @@ std::shared_ptr<Node> Trees::buildXTree(const RankerTable& ranker, const Shortle
     std::set<char> S;
     char sigma;
     for(int i = 0; i < static_cast<int>(text.size()); i++){
-        parent = -69;
+        parent = -1;
         for(char a : shortlex.alphabet){
             x_rank = ranker.getX(i, a);
             if(x_rank > parent) parent = x_rank;
@@ -58,16 +58,17 @@ std::shared_ptr<Node> Trees::buildXTree(const RankerTable& ranker, const Shortle
             }
 
             // line 12: T_X(T).r(i) <- i
+            nodes[i] = std::make_shared<Node>(i, i, 0);
             nodes[i]->r = i;
 
-             // line 13: while s'_p is not empty
+            // line 13: while s'_p is not empty
             while(!sp_p.empty()){
                 // line 14: S <- peek(s'_p)
                 S = sp_p.back();
 
                 // line 15: sigma = arg min (R_X(T, r(i), c))
                 min_x_rank = -1;
-                for(char c : S){
+                for(char c : S) {
                     x_rank = ranker.getX(nodes[i]->r, c);
                     if (min_x_rank == -1 || x_rank < min_x_rank) { 
                         min_x_rank = x_rank;
@@ -78,6 +79,7 @@ std::shared_ptr<Node> Trees::buildXTree(const RankerTable& ranker, const Shortle
                 // line 16: T_X(T).r(i) <- R_X(T, T_X(T).r(i), sigma)
                 nodes[i]->r = ranker.getX(nodes[i]->r, sigma);
                 if (nodes[i]->r == RankerTable::INF) {
+                    sp_p.clear();
                     break;
                 }
 
@@ -88,12 +90,13 @@ std::shared_ptr<Node> Trees::buildXTree(const RankerTable& ranker, const Shortle
                 }
             }
 
-            // T_X(`T`).`chld`(parent) <- [i, i)
+            // line 18: implemented within line 19
         }
-        // extend end point of T_X(`T`).`chld`(parent) by one
+
+        // line 19: extend end point of T_X(`T`).chld(parent) by one
         if(nodes.count(i) != 0){
             nodes[i]->parent = nodes[parent];
-            nodes[parent]->children.push_back(nodes[i]);
+            nodes[parent]->children.push_back(nodes[i]);    // line 18: T_X(T).chld(parent) <- [i, i)
             std::cout << "Set parent of " << i << " to " << parent << std::endl;
         }
     }
@@ -103,7 +106,7 @@ std::shared_ptr<Node> Trees::buildXTree(const RankerTable& ranker, const Shortle
         if(pr.second->parent == nullptr && pr.second != root){
             pr.second->parent = root;
             root->children.push_back(pr.second);
-            std::cout << "Set parent of " << pr.first << " to " << std::numeric_limits<int>::max() << std::endl;
+            std::cout << "Set parent of " << pr.first << " to " << RankerTable::INF << std::endl;
         }
     }
 
@@ -122,7 +125,7 @@ std::shared_ptr<Node> Trees::buildXTree(const RankerTable& ranker, const Shortle
 std::shared_ptr<Node> Trees::buildYTree(const RankerTable& ranker, const ShortlexResult& shortlex, const std::string& text) {
     std::shared_ptr<Node> root = std::make_shared<Node>(-1, -1, 0);
     std::unordered_map<int, std::shared_ptr<Node>> nodes;
-    nodes.insert({{-1, root}});
+    nodes[-1] = root;
     
     std::cout << "Building Y-tree...\n";
 
@@ -144,53 +147,69 @@ std::shared_ptr<Node> Trees::buildYTree(const RankerTable& ranker, const Shortle
     }
 
     // ln 7-21
-    int parent; int y_rnk; int max_y_rnk;
+    int parent; int y_rank; int max_y_rank;
     std::vector<std::set<char>> sp_p;
-    std::unordered_map<int, int> r; std::set<char> S;
-    unsigned char sig;
+    std::set<char> S;
+    unsigned char sigma;
     for(int i = static_cast<int>(text.size()); i > 0; i--){
-        parent = std::numeric_limits<int>::max();
+        parent = RankerTable::INF;
         for(auto a : shortlex.alphabet){
-            y_rnk = ranker.getY(i, a);
-            if(y_rnk < parent) parent = y_rnk;
+            y_rank = ranker.getY(i, a);
+            if(y_rank < parent) parent = y_rank;
         }
 
         if(parent != -1) parent--;
 
         // ln 9-18
         if(nodes.count(parent) == 0){
-            nodes.insert({{parent, std::make_shared<Node>(parent, parent, 0)}});
+            nodes[parent] = std::make_shared<Node>(parent, parent, 0);
             std::cout << "Generate new node " << parent << std::endl;
 
-            // Copy out s_p (ln 11)
-            for(int j = 0; j < s_p.size(); j++){
-                s = s_p.at(j);
-                sp_p.push_back(s);
+            // line 11: s'_p <- copy(s_p)
+            for(auto entry = s_p.begin(); entry != s_p.end(); entry++){
+                sp_p.push_back(*entry);
             }
 
-            r.insert({{i, i}});
-            while(!sp_p.empty()){ // ln 13-17
+            // line 12: T_Y(T).r(i) <- i
+            nodes[i] = std::make_shared<Node>(i, i, 0);
+            nodes[i]->r = i;
+
+            // line 13: while s'_p is not empty
+            while(!sp_p.empty()){
+                // line 14: S <- peek(s'_p)
                 S = sp_p.back();
-                max_y_rnk = std::numeric_limits<int>::max();
-                for(auto c : S){
-                    y_rnk = ranker.getY(r.find(i)->second, c);
-                    if(max_y_rnk == std::numeric_limits<int>::max() || y_rnk > max_y_rnk){ max_y_rnk = y_rnk; sig = c; }
+
+                // line 15: sigma = arg min (R_X(T, r(i), c))
+                max_y_rank = RankerTable::INF;
+                for(char c : S) {
+                    y_rank = ranker.getY(nodes[i]->r, c);
+                    if (max_y_rank == RankerTable::INF || y_rank > max_y_rank) {
+                        max_y_rank = y_rank;
+                        sigma = c;
+                    }
                 }
-                r.find(i)->second = ranker.getY(r.find(i)->second, sig);
-                if (r[i] < 0) {
+
+                // line 16: T_Y(T).r(i) <- R_Y(T, T_Y(T).r(i), sigma)
+                nodes[i]->r = ranker.getY(nodes[i]->r, sigma);
+                if (nodes[i]->r < 0) {
+                    sp_p.clear();
                     break;
                 }
 
-                sp_p.back().erase(sig);
-                if(sp_p.back().empty()){ sp_p.pop_back(); }
+                // line 17: pop sigma from s'_p
+                sp_p.back().erase(sigma);
+                if(sp_p.back().empty()) {
+                    sp_p.pop_back();
+                }
             }
 
-            // T_Y(`T`).`chld`(parent) <- [i, i)
+            // line 18: implemented within line 19
         }
-        // extend end point of T_Y(`T`).`chld`(parent) by one
+
+        // line 19: extend end point of T_Y(T).chld(parent) by one
         if(nodes.count(i) != 0){
-            nodes.find(i)->second->parent = nodes.find(parent)->second;
-            nodes.find(parent)->second->children.push_back(nodes.find(i)->second);
+            nodes[i]->parent = nodes[parent];
+            nodes[parent]->children.push_back(nodes[i]);    // line 18: T_Y(T).chld(parent) <- [i, i)
             std::cout << "Set parent of " << i << " to " << parent << std::endl;
         }
     }
